3e031e1b5c85797da5cd5af0d0d037bfacc83131
Akshat Khandelwal <akshatk1001@gmail.com>
2026-01-07 10:00:01 -0800

    up to #5. next: create removeCharacter

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import MyApp from "./MyApp.jsx";
+import "./main.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+// container
+const container = document.getElementById("root");
+
+// create root
+const root = ReactDOMClient.createRoot(container);
+
+// initial render
+root.render(<MyApp />);
