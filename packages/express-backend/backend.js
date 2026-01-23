cba050d381f03d95bf6c82e310d4ffc0a2c6da49
Akshat Khandelwal <akshatk1001@gmail.com>
2026-01-22 23:57:44 -0800

    only update characters list if POST request correctly runs and returns 201 status

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -93,7 +93,7 @@ app.get("/users/:id", (req, res) => {
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
-  res.send();
+  res.sendStatus(201);
 });
 
 app.delete("/users/:id", (req, res) => {
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -14,7 +14,13 @@ function MyApp() {
 
   function updateList(person) {
     postUser(person)
-      .then(() => setCharacters([...characters, person]))
+      .then((res) => {
+        if (res.status === 201) {
+          setCharacters([...characters, person]);
+        } else {
+          console.log(res.status);
+        }
+      })
       .catch((error) => {
         console.log(error);
       });
